8c303a75ab5d050f97e5d94ed2f4670b2b45d1ef
Akmuhammet Baymuhammedov <akmuhammet_baymuhammedov@epam.com>
2023-01-04 00:42:37 +0200

    adapted waiting message

--- wait_4_deploy.sh
+++ wait_4_deploy.sh
@@ -2,5 +2,5 @@
 
 while [ "$(kubectl get pods -l $1 -n $2 -o jsonpath='{.items[*].status.containerStatuses[0].ready}')" != "true" ]; do
    sleep 5
-   echo "Waiting for $3 to be ready."
+   echo "Waiting for $3 to be ready..."
 done
